42e140f7b62085343963a9d4b1ae397faa81159a
Vincent Van Schependom <vincent.van.schependom@gmail.com>
2024-03-01 21:50:50 +0100

    Aanpassingen Arne aan unit tests

--- Practicum 1/src/FileTest.java
+++ Practicum 1/src/FileTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.*;
 
+import java.beans.Transient;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
@@ -66,7 +67,7 @@ public class FileTest {
     }
 
     @Test
-    public void setName_LegalCase() {
+    public void setName_LegalCase() throws NotAuthorizedException {
         // Changing the name of a file, modification time is changed
         file1.changeName("New-Name");
         Assertions.assertEquals("New-Name",file1.getName());
@@ -74,7 +75,7 @@ public class FileTest {
     }
 
     @Test
-    public void setName_IllegalCase1(){
+    public void setName_IllegalCase1() throws NotAuthorizedException {
         // Changing the name of a file into an illegal name, we expect the name to not change (total programming).
         // The modification is expected to still be null.
         file1.changeName("$#hjfbal$$"); // special characters not allowed
@@ -83,7 +84,7 @@ public class FileTest {
     }
 
     @Test
-    public void setName_IllegalCase2(){
+    public void setName_IllegalCase2() throws NotAuthorizedException {
         // Changing the name of a file into an illegal name, we expect the name to not change (total programming).
         // The modification is expected to still be null.
         file1.changeName("hjf bal"); // space not allowed
@@ -100,7 +101,7 @@ public class FileTest {
     }
 
     @Test
-    public void changeNameWithSameName() {
+    public void changeNameWithSameName() throws NotAuthorizedException {
         // We expect the name to not change and the modification time to be null because it has not been set (total programming).
         file3.changeName("Name3");
         Assertions.assertEquals("Name3", file3.getName());
@@ -108,7 +109,7 @@ public class FileTest {
     }
 
     @Test
-    public void enlargeSize(){
+    public void enlargeSize() throws NotAuthorizedException {
         // We only test legal cases because the size was implemented via nominal programming.
         // The modification time should now be set and not be equal to null.
         file1.enlarge(23);
@@ -116,26 +117,9 @@ public class FileTest {
         Assertions.assertNotNull(file1.getModificationTime());
     }
 
-    @Test
-    public void setWritable_true(){
-        // We only test legal cases because the writeable was implemented via nominal programming.
-        // The modification time should now be set and not be equal to null.
-        file1.setWriteable(false);
-        Assertions.assertFalse(file1.isWriteable());
-        Assertions.assertNull(file1.getModificationTime());
-    }
 
     @Test
-    public void setWritable_false(){
-        // We only test legal cases because the writeable was implemented via nominal programming.
-        // The modification time should now be set and not be equal to null.
-        file3.setWriteable(false);
-        Assertions.assertFalse(file3.isWriteable());
-        Assertions.assertNull(file3.getModificationTime());
-    }
-
-    @Test
-    public void shortenSize(){
+    public void shortenSize() throws NotAuthorizedException {
         // We only test legal cases because the size was implemented via nominal programming.
         // The modification time should now be set and not be equal to null.
         file1.shorten(15);
@@ -144,7 +128,40 @@ public class FileTest {
     }
 
     @Test
-    public void hasOverlappingUsagePeriod1() throws InterruptedException {
+    public void enlargeSizeNotWriteable() {
+        // We test if a non-writeable file throws an exception when trying to modify it
+        Assertions.assertThrows(NotAuthorizedException.class, () ->
+                file2.enlarge(20));
+    }
+
+    @Test
+    public void shortenSizeNotWriteable() {
+        // We test if a non-writeable file throws an exception when trying to modify it
+        Assertions.assertThrows(NotAuthorizedException.class, () ->
+                file2.shorten(20));
+    }
+
+
+    @Test
+    public void setWritable_true(){
+        // We only test legal cases because the writeable was implemented via nominal programming.
+        // The modification time should not be set and be equal to null.
+        file1.setWriteable(false);
+        Assertions.assertFalse(file1.isWriteable());
+        Assertions.assertNull(file1.getModificationTime());
+    }
+
+    @Test
+    public void setWritable_false(){
+        // We only test legal cases because the writeable was implemented via nominal programming.
+        // The modification time should not be set and be equal to null.
+        file3.setWriteable(false);
+        Assertions.assertFalse(file3.isWriteable());
+        Assertions.assertNull(file3.getModificationTime());
+    }
+
+    @Test
+    public void hasOverlappingUsagePeriod1() throws InterruptedException, NotAuthorizedException {
         // Both files are changed so they both have a use period and the periods should overlap.
         TimeUnit.MILLISECONDS.sleep(2); // Wait, otherwise the usage period isn't significant enough.
         file1.enlarge(30);
@@ -155,14 +172,14 @@ public class FileTest {
     }
 
     @Test
-    public void hasOverlappingUsagePeriod2(){
+    public void hasOverlappingUsagePeriod2() throws NotAuthorizedException {
         // Only one file is changed, so we expect false because usage period of file3 is not defined (yet).
         file1.enlarge(30);
         Assertions.assertFalse(file1.hasOverlappingUsagePeriod(file3));
     }
 
     @Test
-    public void hasOverlappingUsagePeriod3(){
+    public void hasOverlappingUsagePeriod3() throws NotAuthorizedException {
         file1.shorten(10); // Usage period of file1 ends
         File newFile = new File("This-file-is-created-after-file1-was-modified",20,true); // Usage period of newFile starts
         newFile.enlarge(20);
